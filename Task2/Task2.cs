33ed775bee00e5ccbe54e23f3aece2f0378a3a28
LebedevKo <sok-is@mail.ru>
2023-05-23 14:19:22 +0400

    1

---
+++ Task2/Task2.cs
@@ -0,0 +1,23 @@
+﻿// сравнивает два числа
+
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max;
+if (number1 > number2)
+{
+     max = number1 ;
+    Console.WriteLine("Максимальное число: " + max);
+
+}
+else if (number1 < number2)
+{
+     max = number2;
+    Console.WriteLine("Максимальное число: " + max );
+}
+else
+{
+    Console.WriteLine("Оба числа равны.");
+}
---
+++ Task8/Task8.cs
@@ -0,0 +1,12 @@
+﻿// четные числа от 1 до N
+
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Четные числа от 1 до N" );
+
+for (int i = 2; i <= N; i += 2)
+    
+Console.WriteLine(i);
+
+
